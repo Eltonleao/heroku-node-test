63e1b3ab253b2c450cf060725f83b07327d62ae6
hello_universe <45052176+Eltonleao@users.noreply.github.com>
2018-12-30 20:52:43 -0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("<style>body{background:skyblue;}*{color: white;text-align:center;font-size:100px}</style><h1>Hello Universe</h1>");
+    res.end("<style>body{background:skyblue;}*{color: white;text-align:center;font-size:100px}</style><h1>Why did the chicken crossed the universe?</h1>");
 });
 
 
