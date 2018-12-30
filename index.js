0e438a5881f552a340a20d4fd19685d9e81020a6
hello_universe <45052176+Eltonleao@users.noreply.github.com>
2018-12-30 20:34:51 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end("<style>body{background:skyblue;}*{color: white;text-align:center;font-size:100px}</style><h1>Hello Universe</h1>");
+});
+
+
+var port=Number(process.env.PORT || 9000);
+server.listen(port);
