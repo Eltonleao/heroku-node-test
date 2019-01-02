029e7c5cd45c4805775508e4e56b2aa5a2793f6c
hello_universe <45052176+Eltonleao@users.noreply.github.com>
2019-01-02 10:53:26 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end("<style>body{background:skyblue;}*{color: white;text-align:center;font-size:100px}</style><h1>Testando fs</h1>");
+});
+console.log("testando writeFile");
+fs.writeFileSync("writeMe.txt", 'Tudo ok');
+var writeMe = fs.readFileSync('writeMe.txt', 'utf8');
+console.log(writeMe);
+
+var port=Number(process.env.PORT || 9000);
+server.listen(port);
