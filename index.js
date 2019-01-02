51618f4c6e45e8a3012617f51104c9d7164597ee
hello_universe <45052176+Eltonleao@users.noreply.github.com>
2019-01-02 15:48:52 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 var http = require('http');
 var express = require('express');
-var server = express();
+var app = express();
 var fs = require('fs');
-var count = 0;
 
-server.get('/', function(req, res){
+app.get('/', function(req, res){
     res.send('homepage');
     fs.writeFileSync("writeMe.txt", "writed!")
-    console.log('request '+count);
-    count ++;
 });
 
-
-
 var port=Number(process.env.PORT || 9000);
-server.listen(port);
+app.listen(port);
