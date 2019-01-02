8d1484f6ed95f74beb6307f6cd9e7ad8aecd0c3e
hello_universe <45052176+Eltonleao@users.noreply.github.com>
2019-01-02 10:58:11 -0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("<style>body{background:skyblue;}*{color: white;text-align:center;font-size:100px}</style><h1>Testando fs</h1>");
+    res.end("<style>body{background:skyblue;}*{color: white;text-align:center;font-size:100px}</style><h1>Changed</h1>");
 });
 console.log("testando writeFile");
 fs.writeFileSync("writeMe.txt", 'Tudo ok');
